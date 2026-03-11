4a1c84f6dff5aeb0ec34f95cef1fb0a510ea474e
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com.>
2026-03-11 18:13:34 +0800

    update

--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -1,5 +1,9 @@
+import { prisma } from "@/lib/prisma";
+
 export async function GET() {
-  return Response.json({ message: "GET request  movie received" });
+  const movies = await prisma.movie.findMany({ take: 10 });
+
+  return Response.json({ movies });
 }
 export async function POST() {
   return Response.json({ message: "POST request movie received" });
