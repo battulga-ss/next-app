dba5aaac09f8e5ff1a578caeb3f9514c9399debd
back-02-04 <back-02-04@Erxess-Mac-mini.local>
2026-03-11 19:44:57 +0800

    123

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,6 @@
 import "dotenv/config";
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
+
 export { prisma };
